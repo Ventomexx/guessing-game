388b772b4fbcf61a0b0d52bac8b8039ebe539c8b
Vadim <108796644+Ventomexx@users.noreply.github.com>
2023-02-22 00:53:36 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.value = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.value = Math.round(this.min + (this.max - this.min) / 2);
     }
 
     guess() {
-
+      return this.value;
     }
 
     lower() {
-
+      game.setRange(this.min, this.value);
     }
 
     greater() {
-
+      game.setRange(this.value, this.max);
     }
 }
 
